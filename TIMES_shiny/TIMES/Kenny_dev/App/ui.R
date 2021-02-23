be02d4cc9cccd34ca436b5f2e8388e50c3e285f7
Kenny-EECA <Kenny.Graham@eeca.govt.nz>
2021-02-24 06:51:51 +1300

    Cleaning the UI code

--- TIMES_shiny/TIMES/Kenny_dev/App/ui.R
+++ TIMES_shiny/TIMES/Kenny_dev/App/ui.R
@@ -14,6 +14,7 @@ library(shinyWidgets) # For the fancy radio buttons
 # Source script that loads data
 ## This is where the .rda file is loaded (and where the hierarchy dataframe is created)
 source("data/load_data.R")
+# Load the plot functions 
 source("funtions.R")
 
 # Start of the user interface code
@@ -222,30 +223,6 @@ ui <- navbarPage(
               )
             )
             
-            # fluidRow(
-            #   
-            #   column(
-            #     
-            #     width = 12,
-            #     
-            #     h3("Kea"),
-            #     
-            #     highchartOutput("transport_kea")
-            #     
-            #   ),
-            #   
-            #   column(
-            #     
-            #     width = 12,
-            #     
-            #     h3("Tui"),
-            #     
-            #     highchartOutput("transport_tui")
-            #     
-            #   )
-            #   
-            # )
-            
             
           ),
           
@@ -303,142 +280,7 @@ ui <- navbarPage(
   ),  
   
   
-  
-  
-  
-  
-  
-  
-  
-  # # Data Explorer tab
-  # tabPanel(
-  #   
-  #   "Data Explorer",
-  #   
-  #   # The sidebarLayout is a specific built-in layout type which takes two arguments: a sidebarPanel and a mainPanel.
-  #   sidebarLayout(
-  #     
-  #     sidebarPanel = sidebarPanel(
-  #       
-  #       width = 3,
-  #       
-  #       # This is where the drop downs are inserted in to the UI. They are created dynamically on the server side.
-  #       uiOutput("drop_downs")
-  #       
-  #     ),
-  #     
-  #     mainPanel = mainPanel(
-  #       
-  #       tabsetPanel(
-  #         
-  #         id = "tabs", # This is needed so that we can reference when someone clicks a tab
-  #         
-  #         # type = "pills",
-  #         
-  #         tabPanel(
-  #           
-  #           "Overview",
-  #           
-  #           value = "Overview", # This is the value (of input$tabs) returned when the user clicks the 'Overview' tab
-  #           
-  #           fluidRow(
-  #             
-  #             # Again, these are the chart type buttons. Have turned off the percent button here as it doesn't make sense for how it is currently set up
-  #             column(
-  #               width = 3,
-  #               # radioButtons("chart_type_assumptions", "", choices = c("line", "column", "area"), inline = TRUE)
-  #               radioGroupButtons(
-  #                 inputId = "chart_type_overview",
-  #                 label = "",
-  #                 choices = c(
-  #                   `<i class="fa fa-line-chart" aria-hidden="true"></i>` = "line",
-  #                   `<i class='fa fa-bar-chart'></i>` = "column",
-  #                   `<i class='fa fa-area-chart'></i>` = "area"#,
-  #                   # `<i class='fa fa-percent'></i>` = "column_percent"
-  #                 )
-  #               )
-  #             ),
-  #             
-  #             # Plot outputs
-  #             column(
-  #               
-  #               width = 12,
-  #               
-  #               h3("Kea"),
-  #               
-  #               highchartOutput("overview_kea")
-  #               
-  #             ),
-  #             
-  #             column(
-  #               
-  #               width = 12,
-  #               
-  #               h3("Tui"),
-  #               
-  #               highchartOutput("overview_tui")
-  #               
-  #             )
-  #             
-  #           )
-  #         ),
-  #         
-  #         tabPanel(
-  #           
-  #           "Transport",
-  #           
-  #           value = "Transport",
-  #           
-  #           fluidRow(
-  #             
-  #             column(
-  #               
-  #               width = 12,
-  #               
-  #               h3("Kea"),
-  #               
-  #               highchartOutput("transport_kea")
-  #               
-  #             ),
-  #             
-  #             column(
-  #               
-  #               width = 12,
-  #               
-  #               h3("Tui"),
-  #               
-  #               highchartOutput("transport_tui")
-  #               
-  #             )
-  #             
-  #           )
-  #         ),
-  #         
-  #         tabPanel(
-  #           
-  #           "Industry",
-  #           
-  #           value = "Industry"
-  #           
-  #         ),
-  #         
-  #         tabPanel("Residential"),
-  #         
-  #         tabPanel(
-  #           
-  #           "Emissions",
-  #           
-  #           value = "Emissions"
-  #           
-  #         )
-  #         
-  #       )
-  #       
-  #     )
-  #     
-  #   )
-  #   
-  # ),
+
   
   # These are CSS files that are needed for displaying the fontawesome icons
   header = tags$head(
