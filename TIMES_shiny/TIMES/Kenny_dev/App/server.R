5179bf1a81588aaafb7e7815ee74c6b48dd84bf5
Kenny-EECA <Kenny.Graham@eeca.govt.nz>
2021-02-24 19:02:19 +1300

    Added download bottom per Anand suggestion and updated design

--- TIMES_shiny/TIMES/Kenny_dev/App/server.R
+++ TIMES_shiny/TIMES/Kenny_dev/App/server.R
@@ -48,12 +48,11 @@ server <- function(input, output, session){
   output$drop_downs <- renderUI({
     
     tagList(
-      selectInput("subsector", "Subsector", choices = c("All Sectors")),# unique(filtered_dropdowns()$Subsector))),
-      selectInput("enduse", "End use", choices = unique(filtered_dropdowns()$Enduse)),
-      selectInput("tech", "Technology", choices = unique(filtered_dropdowns()$Technology)),
-      selectInput("unit", "Unit", choices = unique(filtered_dropdowns()$Unit))
+      selectInput("subsector", label = NULL,  choices = c("All Sectors")),# unique(filtered_dropdowns()$Subsector))),
+      selectInput("enduse", label = NULL, choices = unique(filtered_dropdowns()$Enduse)),
+      selectInput("tech", label = NULL, choices = unique(filtered_dropdowns()$Technology)),
+      selectInput("unit", label = NULL, choices = unique(filtered_dropdowns()$Unit))
 
-      
     )
     
   })
